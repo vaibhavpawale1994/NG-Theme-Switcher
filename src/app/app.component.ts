3a4c3ae11737843a9aa5cb72fcadaa51a8d3d359
Vaibhav <vaibhavpawale19@gmail.com>
2023-11-14 22:37:22 +0530

    ng theme switcher

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Ng-theme-switcher';
+  public isLightTheme = true;
+
+  onThemeSwitchChange() {
+    this.isLightTheme = !this.isLightTheme;
+
+    document.body.setAttribute(
+      'data-theme',
+      this.isLightTheme ? 'light' : 'dark'
+    );
+  }
+}
